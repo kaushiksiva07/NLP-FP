55cd3614444607697eabbabafbefbb98fa271dc0
kaushiksiva07 <77030019+kaushiksiva07@users.noreply.github.com>
2022-11-29 19:27:32 -0600

    Update run.py

--- run.py
+++ run.py
@@ -59,13 +59,13 @@ def main():
         # from the loaded dataset
         eval_split = 'train'
     else:
-        default_datasets = {'qa': ('squad',), 'nli': ('snli',), 'adv': ('adversarial_qa', 'adversarialQA')}
+        default_datasets = {'qa': ('squad',), 'nli': ('snli',)}
         dataset_id = tuple(args.dataset.split(':')) if args.dataset is not None else \
             default_datasets[args.task]
         # MNLI has two validation splits (one with matched domains and one with mismatched domains). Most datasets just have one "validation" split
         eval_split = 'validation_matched' if dataset_id == ('glue', 'mnli') else 'validation'
         # Load the raw data
-        dataset = datasets.load_dataset(*dataset_id)
+        dataset = datasets.load_dataset('adversarial_qa', 'adversarialQA')
     
     # NLI models need to have the output label count specified (label 0 is "entailed", 1 is "neutral", and 2 is "contradiction")
     task_kwargs = {'num_labels': 3} if args.task == 'nli' else {}
